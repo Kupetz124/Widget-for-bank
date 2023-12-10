0a53c7529c0fa4696efcd6a97ee2f9b4ec300830
__Vitaliy__ <akkauntmoy124@gmail.com>
2023-12-10 15:43:22 +0300

    Functionality update

--- src/services.py
+++ src/services.py
@@ -1,15 +1,17 @@
 import pandas as pd
 
+from src.decorators import log
 
-def search_string(path: str, row: str) -> pd.DataFrame:
+
+@log("my_search")
+def search_string(df: pd.DataFrame, row: str) -> pd.DataFrame:
     """
     Возвращает DataFrame с транзакциями, в описании или категории которых есть искомая строка.
-    :param path: Путь к файлу с транзакциями.
+    :param df: DataFrame с транзакциями.
     :param row:Искомая строка или слово.
     :return: DataFrame с транзакциями, содержащими искомую строку.
     """
 
-    df = pd.read_excel(path)
     df = df[df["Описание"].str.contains(row) | df["Категория"].str.contains(row)]
 
     return df

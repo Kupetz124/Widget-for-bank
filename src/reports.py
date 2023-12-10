da052b14528eb9a276f4a7430aa61f984cf8e19e
__Vitaliy__ <akkauntmoy124@gmail.com>
2023-12-10 15:42:50 +0300

    Functionality update

--- src/reports.py
+++ src/reports.py
@@ -5,16 +5,25 @@ import pandas as pd
 
 from src.decorators import log
 
-pd.set_option("display.max_rows", None)
-pd.set_option("display.max_columns", None)
+# pd.set_option("display.max_rows", None)
+# pd.set_option("display.max_columns", None)
+
+DATE = "2021-12-31 16:44:00"
 
 
 @log("reports")
 def spending_by_category(df: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
+    """
+    Возвращает траты по заданной категории за последние 3 месяца (от переданной даты).
+    :param df: DataFrame с транзакциями.
+    :param category: Искомая категория расходов.
+    :param date:Искомая дата, если не указана, берётся текущая.
+    :return:DataFrame с транзакциями
+    """
     if date is None:
         date_now = dt.datetime.now().strftime("%d.%m.%Y")
     else:
-        date_now = dt.datetime.strptime(date, "%d.%m.%Y").strftime("%d.%m.%Y")
+        date_now = dt.datetime.strptime(date, "%Y-%m-%d %H:%M:%S").strftime("%d.%m.%Y")
 
     df["Дата платежа"] = pd.to_datetime(df["Дата платежа"], dayfirst=True)
 
